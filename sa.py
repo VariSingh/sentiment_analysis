e5fa7e37aa7eb43a2237ca15761b265f4e38337c
Varinder Singh <varindersingh271990@gmail.com>
2018-07-29 19:01:57 +0530

    read data from files and lemmatize(in progress)

--- sa.py
+++ sa.py
@@ -1,6 +1,18 @@
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
 
-sentense = "This is a sample sentense"
-tokens = nltk.word_tokenize(sentense)
-print(tokens)
+
+lemmatizer = WordNetLemmatizer()
+no_of_lines = 10000000
+
+
+def create_lexicon(pos,neg):
+        lexicon = []
+        for file in [pos,neg]:
+            with open(file,'r') as f:
+                contents = f.readlines()
+                for lines in contents[:no_of_lines]:
+                    #all_words = word_tokenize(l)
+                    #lexicon +=list(all_words)
