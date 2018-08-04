90b160a5eb321e16fbc0fea477dec5f7de8b530d
Varinder Singh <varindersingh271990@gmail.com>
2018-08-04 21:47:25 +0530

    bug fixed in features list

--- sa.py
+++ sa.py
@@ -12,22 +12,21 @@ no_of_lines = 10000000
 
 
 def create_lexicon(pos,neg):
-        lexicon = []
-        for file in [pos,neg]:
-            with open(file,'r') as f:
-                contents = f.readlines()
-                for lines in contents[:no_of_lines]:
-                    all_words = word_tokenize(lines.lower())
-                    lexicon +=list(all_words)
-
-        lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
-        word_counts = Counter(lexicon)
-        l2 = []
-        for word in word_counts:
-            if 1000 >  word_counts[word] > 50:
-                l2.append(word)
-        print(len(l2))
-        return l2
+    lexicon = []
+    for file in [pos,neg]:
+        with open(file,'r') as f:
+            contents = f.readlines()
+            for lines in contents[:no_of_lines]:
+                all_words = word_tokenize(lines.lower())
+                lexicon +=list(all_words)
+    lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+    word_counts = Counter(lexicon)
+    l2 = []
+    for word in word_counts:
+        if 1000 >  word_counts[word] > 50:
+            l2.append(word)
+    print(len(l2))
+    return l2
 
 
 #classify feature sets
@@ -57,18 +56,20 @@ def create_feature_sets_and_labels(pos,neg,test_size=0.1):
     features += sample_handling('pos.txt',lexicon,[1,0])
     features += sample_handling('neg.txt',lexicon,[0,1])
     random.shuffle(features)
+
     testing_size = int(test_size*len(features))
+    train_x = list([i[0] for i in features][:-testing_size])
+    train_y = list([i[1] for i in features][:-testing_size])
 
-    train_x = list(features[:,0][:-testing_size])
-    train_y = list(features[:,1][:-testing_size])
-
-    test_x = list(features[:,0][-testing_size:])
-    test_y = list(featuers[:,1][-testing_size:])
-
+    test_x = list([i[0] for i in features][-testing_size:])
+    test_y = list([i[1] for i in features][-testing_size:])
     return train_x,train_y,test_x,test_y
 
 
 if __name__ == '__main__':
     train_x,train_y,test_x,test_y =  create_feature_sets_and_labels('pos.txt','neg.txt')
+
+#result = create_feature_sets_and_labels('pos.txt','neg.txt')
+#print(result)
     with open('sentiment set.pickle','wb') as f:
         pickle.dump([train_x,train_y,test_x,test_y],f)
